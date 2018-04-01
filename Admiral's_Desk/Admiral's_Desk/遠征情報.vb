05df6c8eaf70e6ae992aa7db8cb7af1f411a532c
masteralice3104 <alicemargatroid@alicemargatroid.com>
2018-04-01 18:34:40 +0900

    遠征の残り時間表示に「帰還済み」が表示できるようになり、帰還済みの場合時刻が増えていく不具合を修正

--- Admiral's_Desk/Admiral's_Desk/遠征情報.vb
+++ Admiral's_Desk/Admiral's_Desk/遠征情報.vb
@@ -20,11 +20,17 @@
                     Dim 現在時刻 = DateTimeOffset.Now
                     Dim 帰還時刻 = DateTimeOffset.FromUnixTimeSeconds(MyDataClass.MyPort(cnt).api_mission(2) / 1000).ToLocalTime
                     Dim 残り時間 = 帰還時刻 - 現在時刻
+                    Dim 残り時間表示 As String = 残り時間.ToString("hh\:mm\:ss")
+
+                    If 残り時間.Seconds < 0 Then
+                        残り時間表示 = "帰還済み"
+                    End If
+
 
                     Dim 艦隊名称 = MyDataClass.MyPort(cnt).api_name
                     Dim 遠征名称 = Component.KancolleMissionIDSearch(MyDataClass.MyPort(cnt).api_mission(1))
 
-                    遠征情報表示.Rows.Add(艦隊名称, 遠征名称, 残り時間.ToString("hh\:mm\:ss"), String.Format("{0:MM/dd HH:mm:ss}", 帰還時刻))
+                    遠征情報表示.Rows.Add(艦隊名称, 遠征名称, 残り時間表示, String.Format("{0:MM/dd HH:mm:ss}", 帰還時刻))
                 End If
             Next
         End If
